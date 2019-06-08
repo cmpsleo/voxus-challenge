28439d1a77be7f3428b596caa0e6e3e14b1f59c4
Léo Campos <techmuha@gmail.com>
2019-06-08 20:44:24 -0300

    feat(heading): created the responsive heading component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <main>
     <TheHeader />
+
+    <section>
+      <TheHeading />
+    </section>
     
     <TheFooter />
   </main>
@@ -9,11 +13,13 @@
 <script>
 import TheHeader from './components/TheHeader'
 import TheFooter from './components/TheFooter'
+import TheHeading from './components/TheHeading'
 
 export default {
   components: {
     TheHeader,
-    TheFooter
+    TheFooter,
+    TheHeading
   }
 }
 </script>
---
+++ src/components/TheHeading.vue
@@ -0,0 +1,37 @@
+<template>
+  <hgroup class="the-heading">
+    <h1 class="title">Você está a poucos passos de otimizar suas campanhas!</h1>
+    
+    <h2 class="text">Queremos conhecer mais sobre sua empresa para melhorar sua experiência com a Voxus.</h2>
+  </hgroup>
+</template>
+
+<style lang="scss" scoped>
+  .the-heading {
+    text-align: center;
+
+    @include media-query('greaterThan', medium) { margin-bottom: 4.5rem; }
+
+    @include media-query('lessThan', medium) {
+      margin-bottom: 3.5rem;
+      padding: 0 1rem;
+    }
+
+    > .title {
+      padding-bottom: 1.1rem;
+
+      font-weight: 500;
+      color: map-get($gray-color, dark);
+
+      @include media-query('greaterThan', medium) { font-size: 2rem; }
+    }
+
+    > .text {
+      color: map-get($gray-color, normal);
+      font: {
+        size: .875rem;
+        weight: normal;
+      };
+    }
+  }
+</style>
