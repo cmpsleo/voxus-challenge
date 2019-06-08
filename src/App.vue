c5f529ae14df8dbc83e061cec786c2797bfe8db2
Léo Campos <techmuha@gmail.com>
2019-06-08 19:57:50 -0300

    feat(header): made the responsive header component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>Hello World</h1>
+  <TheHeader />
 </template>
+
+<script>
+import TheHeader from './components/TheHeader'
+
+export default {
+  components: {
+    TheHeader
+  }
+}
+</script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="the-header">
+    <figure class="logo">
+      <img class="image" src="@/assets/images/logo-voxus.svg" title="Voxus - Mídia  programática para e-commerces" alt="Voxus - Mídia  programática para e-commerces">
+    </figure>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+  .the-header {
+    width: 100%;
+    height: 3.125rem;
+    padding: 0 1rem;
+    margin-bottom: 3.125rem;
+
+    display: flex;
+    align-items: center;
+
+    background-color: #FFF;
+    border-bottom: 1px solid rgba(45, 58, 64, .1);
+
+    @include media-query('greaterThan', large) {
+      padding: 0 12rem;
+      margin-bottom: 3.75rem;
+    }
+
+    > .logo {
+      > .image { height: 1rem }
+    }
+  }
+</style>
