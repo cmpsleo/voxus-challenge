274fcf4e79399cb2164efab9a6fb269ef786ba86
Léo Campos <techmuha@gmail.com>
2019-06-08 21:04:53 -0300

    feat(FormCard): created the form card component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
 
     <section>
       <TheHeading />
+
+      <FormCard />
     </section>
     
     <TheFooter />
@@ -14,12 +16,25 @@
 import TheHeader from './components/TheHeader'
 import TheFooter from './components/TheFooter'
 import TheHeading from './components/TheHeading'
+import FormCard from './components/FormCard'
 
 export default {
   components: {
     TheHeader,
     TheFooter,
-    TheHeading
+    TheHeading,
+    FormCard
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  section {
+    margin: 0 1rem;
+
+    @include media-query('greaterThan', medium) {
+      width: 620px;
+      margin: 0 auto;
+    }
+  }
+</style>
---
+++ src/components/FormCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="form-card">
+    hello world
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .form-card {
+    padding: 1rem;
+    margin-bottom: 5rem;
+
+    border-radius: 3px;
+    background-color: #FFF;
+    box-shadow: 0px 4px 40px rgba(116, 130, 150, .2);
+
+    @include media-query('greaterThan', medium) { padding: 1.6rem; }
+  }
+</style>
